e496a0863dec7b6388672a1c66e9ca7cd91a0464
mama ima creamanal <47227507+IAmSlonik@users.noreply.github.com>
2022-07-08 15:54:13 +0200

    update LMB

--- GUI/LEWY/Lewy.Designer.cs
+++ GUI/LEWY/Lewy.Designer.cs
@@ -28,17 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.components = new System.ComponentModel.Container();
+            this.bBind = new System.Windows.Forms.Button();
+            this.tbMin = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Ltimer = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.tBind = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.tbMin)).BeginInit();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // bBind
             // 
-            this.groupBox1.Location = new System.Drawing.Point(98, 78);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "lewy";
+            this.bBind.FlatAppearance.BorderSize = 0;
+            this.bBind.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bBind.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bBind.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.bBind.Location = new System.Drawing.Point(150, 29);
+            this.bBind.Name = "bBind";
+            this.bBind.Size = new System.Drawing.Size(75, 23);
+            this.bBind.TabIndex = 0;
+            this.bBind.Text = "brak";
+            this.bBind.UseVisualStyleBackColor = true;
+            this.bBind.Click += new System.EventHandler(this.bBind_Click_1);
+            this.bBind.KeyUp += new System.Windows.Forms.KeyEventHandler(this.bBind_KeyUp);
+            // 
+            // tbMin
+            // 
+            this.tbMin.Location = new System.Drawing.Point(124, 94);
+            this.tbMin.Maximum = 25;
+            this.tbMin.Minimum = 1;
+            this.tbMin.Name = "tbMin";
+            this.tbMin.Size = new System.Drawing.Size(104, 45);
+            this.tbMin.TabIndex = 1;
+            this.tbMin.Value = 1;
+            this.tbMin.Scroll += new System.EventHandler(this.tbMin_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(248, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CPS";
+            // 
+            // Ltimer
+            // 
+            this.Ltimer.Enabled = true;
+            this.Ltimer.Tick += new System.EventHandler(this.Ltimer_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.checkBox1.Location = new System.Drawing.Point(150, 154);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "disabled";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // tBind
+            // 
+            this.tBind.Enabled = true;
+            this.tBind.Interval = 150;
+            this.tBind.Tick += new System.EventHandler(this.tBind_Tick);
             // 
             // Lewy
             // 
@@ -46,17 +105,27 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
             this.ClientSize = new System.Drawing.Size(383, 270);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbMin);
+            this.Controls.Add(this.bBind);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Lewy";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lewy";
+            ((System.ComponentModel.ISupportInitialize)(this.tbMin)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button bBind;
+        private System.Windows.Forms.TrackBar tbMin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer Ltimer;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer tBind;
     }
 }
--- GUI/LEWY/Lewy.cs
+++ GUI/LEWY/Lewy.cs
@@ -7,14 +7,138 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace Clicker.GUI.LEWY
 {
     public partial class Lewy : Form
     {
+        #region dlls
+        [DllImport("User32.Dll", EntryPoint = "PostMessageA")]
+        private static extern bool PostMessage(IntPtr hWnd, uint msg, int wParam, int lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+        #endregion
         public Lewy()
         {
             InitializeComponent();
+        }
+
+        private void tbMin_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = $"{tbMin.Value}";
+        }
+        IntPtr hWnd;
+        public string getActiveWindowName()
+        {
+            try
+            {
+                var activateHandle = GetForegroundWindow();
+
+                Process[] processes = Process.GetProcesses();
+                foreach (Process clsProcess in processes)
+                {
+                    if (activateHandle == clsProcess.MainWindowHandle)
+                    {
+                        string processName = clsProcess.ProcessName;
+                        return processName;
+                    }
+                }
+            }
+            catch { }
+            return null;
+        }
+        private async void Ltimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Ltimer.Interval = 1000 / tbMin.Value;
+            }
+            catch { }
+            if (checkBox1.Text.Contains("Enabled"))
+            {
+                Process[] processes = Process.GetProcessesByName("javaw");
+                foreach(Process process in processes)
+                {
+                    hWnd = FindWindow(null, process.MainWindowTitle);
+                }
+                string currentwindow = getActiveWindowName();
+                if (currentwindow == null)
+                {
+                    return;
+                }
+                if (currentwindow.Contains("javaw"))
+                {
+                    if(MouseButtons == MouseButtons.Left)
+                    {
+                        PostMessage(hWnd, 0x0201, 0, 0);
+                        await Task.Delay(30);
+                        PostMessage(hWnd, 0x0202, 0, 0);
+                    }
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                checkBox1.Text = "Enabled";
+            }
+            else
+            {
+                checkBox1.Text = "Disabled";
+            }
+        }
+
+        private void bBind_Click_1(object sender, EventArgs e)
+        {
+            if (bBind.Text == "brak")
+            {
+                bBind.Text = "***";
+            }
+            else
+            {
+                bBind.Text = "***";
+            }
+        }
+
+        private void bBind_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (bBind.Text == "***")
+            {
+                
+                if (GetAsyncKeyState(Keys.Escape) < 0)
+                {
+                    bBind.Text = "brak";
+                }
+                else { bBind.Text = e.KeyData.ToString(); }
+            }
+        }
+        KeysConverter Key = new KeysConverter();
+
+        private void tBind_Tick(object sender, EventArgs e)
+        {
+            if(bBind.Text != "brak")
+            {
+                if(bBind.Text != "***")
+                {
+                    Keys key = (Keys)Key.ConvertFromString(bBind.Text);
+                    if(GetAsyncKeyState(key) < 0)
+                    {
+                        checkBox1.Checked = !checkBox1.Checked;
+                    }
+                }
+            }
         }
     }
 }
